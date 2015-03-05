a6b8dde4491b4244d02e18eb4e9103bcadff8641
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2015-03-05 10:57:28 +0100

    Comment out old histsize and histfilesize

--- copy_bash.sh
+++ copy_bash.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 echo "Making a copy of your .bashrc file to .bashrc_backup"
-cp .bashrc .bashrc_backup
+cp $1 $1_backup
 
+# Comment out any occurance of HISTSIZE and HISTFILESIZE
+sed -i s/^HISTSIZE\=/#HISTSIZE\=/g $1
+sed -i s/^HISTFILESIZE\=/#HISTFILESIZE\=/g $1
+
+
+echo "Diff of backup and new .bashrc"
+diff $1_backup $1
