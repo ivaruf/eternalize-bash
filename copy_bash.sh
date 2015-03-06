05248e78f69bb035f57c4c5ea9230c84fade5c91
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2015-03-06 08:41:16 +0100

    Add symlink, use home directory, move snippet into script and more output

--- copy_bash.sh
+++ copy_bash.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-echo "Making a copy of your .bashrc file to .bashrc_backup"
-cp $1 $1_backup
+echo "Entering home directoy"
+cd /home/$USER
 
-# Comment out any occurance of HISTSIZE and HISTFILESIZE
+echo "Making a copy of your .bashrc file to .bashrc_backup"
+cp .bashrc .bashrc_backup
+
+echo "Comment out old occurances of HISTSIZE and HISTFILESIZE"
 sed -i s/^HISTSIZE\=/#HISTSIZE\=/g $1
 sed -i s/^HISTFILESIZE\=/#HISTFILESIZE\=/g $1
 
-cat eternal_bash_snippet.txt >> $1
+ETERNAL_BASH_SNIPPET="
+# Eternal bash history.
+# ---------------------
+# Undocumented feature which sets the size to \"unlimited\".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=
+export HISTSIZE=
+export HISTTIMEFORMAT=\"[%F %T] \"
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND=\"history -a; history -c; history -r; \$PROMPT_COMMAND\""
 
-echo "Diff of backup and new .bashrc"
-diff $1_backup $1
+echo "Appending eternal bash snippet to end of .bashrc"
+echo "${ETERNAL_BASH_SNIPPET}" >> .bashrc
+
+echo "Diff of backup and new .bashrc:"
+diff .bashrc_backup .bashrc
+
+echo "Setting new .bashrc as source"
+source .bashrc
+
+DROPBOX_DEFAULT_LOCATION="/home/$USER/Dropbox"
+
+if [ -d "$DROPBOX_DEFAULT_LOCATION" ]; then
+	echo "Dropbox found at default location: $DROPBOX_DEFAULT_LOCATION"
+	echo "Making history-file to dropbox and making symlink"
+	touch ${DROPBOX_DEFAULT_LOCATION}/.bash_eternal_history
+	ln -s ${DROPBOX_DEFAULT_LOCATION}/.bash_eternal_history .bash_eternal_history
+
+else
+	echo "Dropbox not found, not adding symlink"
+fi
