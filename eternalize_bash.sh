250dc5cf359834e4e0d99bb77080ff5d83df785a
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2015-03-17 15:28:47 +0100

    Clean up messages to user (and use spaces instead of tabs)

--- eternalize_bash.sh
+++ eternalize_bash.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-echo "Entering home directoy"
+# go to home directoy
 cd
 
 if [ -f .bashrc ]; then
   echo "Making a copy of your .bashrc file to .bashrc_backup"
   cp .bashrc .bashrc_backup
-else
-  echo ".bashrc not found"
 fi
 
 echo "Comment out old occurances of HISTSIZE and HISTFILESIZE"
@@ -29,7 +27,7 @@ export HISTFILE=~/.bash_eternal_history
 # http://superuser.com/questions/20900/bash-history-loss
 PROMPT_COMMAND=\"history -a; history -c; history -r; \$PROMPT_COMMAND\""
 
-echo "Appending eternal bash snippet to end of .bashrc"
+echo "Appending eternal bash snippet to .bashrc"
 echo "${ETERNAL_BASH_SNIPPET}" >> .bashrc
 
 echo "Setting new .bashrc as source"
@@ -38,12 +36,12 @@ source .bashrc
 DROPBOX_DEFAULT_LOCATION=~/Dropbox/
 
 if [ -d "$DROPBOX_DEFAULT_LOCATION" ]; then
-	echo "Dropbox found at default location: $DROPBOX_DEFAULT_LOCATION"
-	echo "Making history-file to dropbox and making symlink"
-	mkdir -p  ${DROPBOX_DEFAULT_LOCATION}/linux_dot/
-	touch ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history
-	ln -s ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history .bash_eternal_history
-
+  echo "Dropbox found at default location: $DROPBOX_DEFAULT_LOCATION"
+  echo "Adding history-file to dropbox and making symlink:"
+  mkdir -p  ${DROPBOX_DEFAULT_LOCATION}/linux_dot/
+  touch ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history
+  ln -s ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history .bash_eternal_history
+  echo "~/.bash_eternal_history -> ${DROPBOX_DEFAULT_LOCATION}linux_dot/.bash_eternal_history"
 else
-	echo "Dropbox not found, not adding symlink"
+  echo "Dropbox not found, not adding symlink"
 fi
