49b9feaf0f6f1fbc595077c45eb6d1b3c656a0d8
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2015-04-17 21:40:27 +0200

    Rewrite script
    * Don't use links or symlinks in windows
    * grep .bashrc to check if script should run
    * remove "cleanup" of old .bashrc - not really needed

--- eternalize_bash.sh
+++ eternalize_bash.sh
@@ -3,17 +3,13 @@
 # go to home directory
 cd
 
-if [ -f .bash_eternal_history ]; then
-  echo "It looks like script has already been run, no changes will be made."
-  exit 1
-fi
-
 if [ -f .bashrc ]; then
+  if grep -q "bash_eternal_history" ".bashrc"; then
+    echo "It looks like script has already been run, no changes will be made."
+    exit 1
+  fi
   echo "Making a copy of your .bashrc file to .bashrc_backup."
   cp .bashrc .bashrc_backup
-  echo "Comment out old occurances of HISTSIZE and HISTFILESIZE."
-  sed -i s/^HISTSIZE\=/#HISTSIZE\=/g .bashrc
-  sed -i s/^HISTFILESIZE\=/#HISTFILESIZE\=/g .bashrc
 fi
 
 ETERNAL_BASH_SNIPPET="
@@ -24,30 +20,27 @@ ETERNAL_BASH_SNIPPET="
 export HISTFILESIZE=
 export HISTSIZE=
 export HISTTIMEFORMAT=\"[%F %T] \"
-# Change the file location because certain bash sessions truncate .bash_history file upon close.
-# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
-export HISTFILE=~/.bash_eternal_history
-# Force prompt to write history after every command.
-# http://superuser.com/questions/20900/bash-history-loss
+# Force prompt to write history after every command. See 'help history'
 PROMPT_COMMAND=\"history -a; history -c; history -r; \$PROMPT_COMMAND\""
 
-echo "Appending eternal bash snippet to .bashrc."
-echo "${ETERNAL_BASH_SNIPPET}" >> .bashrc
+echo ${ETERNAL_BASH_SNIPPET} >> .bashrc
 
 DROPBOX_DEFAULT_LOCATION=~/Dropbox/
 
 if [ -d "$DROPBOX_DEFAULT_LOCATION" ]; then
   echo "Dropbox found at default location: $DROPBOX_DEFAULT_LOCATION"
-  echo "Adding history-file to dropbox and making link:"
   mkdir -p  ${DROPBOX_DEFAULT_LOCATION}/linux_dot/
-  touch ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history
-  ln ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history .bash_eternal_history
-  echo "Made link ~/.bash_eternal_history -> ${DROPBOX_DEFAULT_LOCATION}linux_dot/.bash_eternal_history."
+  cat .bash_history >> ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history
+  # Check if running windows, symlink will not work there.
+  if [ -n "$WINDIR" ]; then
+    echo "Setting HISTFILE to ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history"
+    echo export HISTFILE=${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history >> .bashrc
+  else
+    echo "Making link ~/.bash_eternal_history -> ${DROPBOX_DEFAULT_LOCATION}linux_dot/.bash_eternal_history."
+    echo export HISTFILE=~/.bash_eternal_history >> .bashrc
+    ln -s ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history .bash_eternal_history
+  fi
 else
-  echo "Dropbox not found, not adding link."
-fi
-
-if [ -f .bash_history ]; then
-  echo "Old history file found, appending it to the new one."
-  cat .bash_history >> .bash_eternal_history
+  echo "Dropbox not found, history file is set to ~/.bash_eternal_history"
+  echo export HISTFILE=~/.bash_eternal_history >> .bashrc
 fi
