1595d76a8fd8726099cab5be05573ca1b3ebbc85
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2015-04-18 11:35:51 +0200

    Use variables consitently

--- eternalize_bash.sh
+++ eternalize_bash.sh
@@ -1,48 +1,58 @@
 #!/bin/bash
 
+BASH_RC=.bashrc
+DROPBOX_DEFAULT_LOCATION=~/Dropbox
+DROPBOX_FOLDER=linux_dot
+HISTFILE_NAME=.bash_eternal_history
+
 # go to home directory
 cd
 
-if [ -f .bashrc ]; then
-  if grep -q "bash_eternal_history" ".bashrc"; then
+if [ -f ${BASH_RC} ]; then
+  if grep -q "${HISTFILE_NAME}" "${BASH_RC}"; then
     echo "It looks like script has already been run, no changes will be made."
     exit 1
   fi
-  echo "Making a copy of your .bashrc file to .bashrc_backup."
-  cp .bashrc .bashrc_backup
+  echo "Making a copy of your ${BASH_RC} file to ${BASH_RC}_backup."
+  cp ${BASH_RC} ${BASH_RC}_backup
 fi
 
 ETERNAL_BASH_SNIPPET="
 # Eternal bash history.
 # ---------------------
-# Undocumented feature which sets the size to \"unlimited\".
+# Undocumented feature which sets the size to 'unlimited'.
 # http://stackoverflow.com/questions/9457233/unlimited-bash-history
 export HISTFILESIZE=
 export HISTSIZE=
 export HISTTIMEFORMAT=\"[%F %T] \"
 # Force prompt to write history after every command. See 'help history'
-PROMPT_COMMAND=\"history -a; history -c; history -r; \$PROMPT_COMMAND\""
+PROMPT_COMMAND=\"history -a; history -c; history -r; \${PROMPT_COMMAND}\""
 
-echo "${ETERNAL_BASH_SNIPPET}" >> .bashrc
+# Important with quotes here, so the snippet will retain line-breaks
+echo "${ETERNAL_BASH_SNIPPET}" >> ${BASH_RC}
 
-DROPBOX_DEFAULT_LOCATION=~/Dropbox
+if [ -d "${DROPBOX_DEFAULT_LOCATION}" ]; then
+  echo "Dropbox found at default location: ${DROPBOX_DEFAULT_LOCATION}"
+  HISTFILE_LOCATION=${DROPBOX_DEFAULT_LOCATION}/${DROPBOX_FOLDER}/${HISTFILE_NAME}
+  mkdir -p  ${DROPBOX_DEFAULT_LOCATION}/${DROPBOX_FOLDER}/
 
-if [ -d "$DROPBOX_DEFAULT_LOCATION" ]; then
-  echo "Dropbox found at default location: $DROPBOX_DEFAULT_LOCATION"
-  mkdir -p  ${DROPBOX_DEFAULT_LOCATION}/linux_dot/
-  cat .bash_history >> ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history
   # Check if running windows, symlink will not work there.
-  if [ -n "$WINDIR" ]; then
-    echo "Windows detected."
-    echo "Setting HISTFILE to ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history"
-    echo export HISTFILE=${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history >> .bashrc
+  if [ -n "${WINDIR}" ]; then
+    echo "Windows detected"
+    echo "Setting HISTFILE to ${HISTFILE_LOCATION} in ${BASH_RC}"
+    echo export HISTFILE=${HISTFILE_LOCATION} >> ${BASH_RC}
   else
-    echo "Making link ~/.bash_eternal_history -> ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history."
-    echo export HISTFILE=~/.bash_eternal_history >> .bashrc
-    ln -s ${DROPBOX_DEFAULT_LOCATION}/linux_dot/.bash_eternal_history .bash_eternal_history
+    echo "Making link ~/${HISTFILE_NAME} -> ${HISTFILE_LOCATION}"
+    touch ${HISTFILE_LOCATION}
+    ln -s ${HISTFILE_LOCATION} ${HISTFILE_NAME}
+    echo "Setting HISTFILE to ~/${HISTFILE_NAME} in ${BASH_RC}"
+    echo export HISTFILE=~/${HISTFILE_NAME} >> ${BASH_RC}
   fi
 else
-  echo "Dropbox not found, history file is set to ~/.bash_eternal_history"
-  cat .bash_history >> ~/.bash_eternal_history
-  echo export HISTFILE=~/.bash_eternal_history >> .bashrc
+  HISTFILE_LOCATION=~/${HISTFILE_NAME}
+  echo "Dropbox not found, history file is set to ${HISTFILE_LOCATION}"
+  echo HISTFILE=${HISTFILE_LOCATION} >> ${BASH_RC}
 fi
+
+echo "Appending .bash_history to ${HISTFILE_LOCATION}"
+cat .bash_history >> ${HISTFILE_LOCATION}
