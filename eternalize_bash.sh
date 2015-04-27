f5aead90ef1b6660cfdc59a7b047818a08c194ec
Ivar Fredriksen <ivar.fredriksen@iterate.no>
2015-04-27 21:46:33 +0200

    wip, make more functions

--- eternalize_bash.sh
+++ eternalize_bash.sh
@@ -15,65 +15,141 @@ _(_  )_  /_/ / /_/ /_(__  )_  / / /
 /_/
 BANNER
 
-# Colors
-red='\033[0;31m'
-NC='\033[0m' # No Color
-
-case $(uname) in
-    Linux) OPERATING_SYSTEM=Linux;;
-    Darwin) OPERATING_SYSTEM=Mac;;
-    *) OPERATING_SYSTEM=Windows;;
-esac
-
-echo -e "${red}${OPERATING_SYSTEM}"
-
-echo "Select and option:"
-select option in "Yes" "No"; do
-    case $option in
-        Yes) break;;
-        No ) exit;;
-    esac
-    echo "Type (number) + enter to select"
-done
-
-#USE if [ -L ${file} to check if symlink ]
-
+# Variables
 BASH_RC=.bashrc
 DROPBOX_DEFAULT_LOCATION=~/Dropbox
 DROPBOX_FOLDER=linux_dot
 HISTFILE_NAME=.bash_eternal_history
 OLD_HISTORY=.bash_history
-VERSION=0.0.1
+CURRENT_HISTFILE=${HISTFILE}
+
+# Colors
+red='\033[0;31m'
+green='\033[0;32m'
+cyan='\033[1;36m'
+colorless='\033[0m'
+
+printf "\nHistory file currently set to:\n"
+printf "${cyan}${CURRENT_HISTFILE}${colorless}"
+if isSymlink ${CURRENT_HISTFILE}; then
+    printf " -> "
+    HISTPATH=$(readlink -f ${CURRENT_HISTFILE})
+    printf ${HISTPATH}
+else
+    HISTPATH=${CURRENT_HISTFILE}
+fi
+
+printf "\n\n"
 
 # go to home directory
 cd
 
+case $(uname) in
+    Linux) OS=Linux;;
+    Darwin) OS=Mac;;
+    *) check_if_windows;;
+esac
+
+function isSymlink() {
+    if [ -L $1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function check_if_windows() {
+    if [ -n "${WINDIR}" ]; then
+        OS=Windows
+    else
+        OS=Unknown
+    fi
+}
+
+printf "${green}Operating system: ${OS}${colorless}\n\n"
+
+function uninstall() {
+    STARTLINE=`grep -n  "# Eternal bash history" ${BASH_RC} | cut -d ":" -f1`
+    ENDLINE=`grep -n  "/${HISTFILE_NAME}" ${BASH_RC} | cut -d ":" -f1`
+
+    printf "${red}Remove the following lines from ${BASH_RC}?${colorless} \n"
+    sed -n "${STARTLINE},${ENDLINE}p" ${BASH_RC}
+    select option in "Proceed" "Cancel"; do
+        case ${option} in
+            Proceed) sed "${STARTLINE},${ENDLINE}d" ${BASH_RC};break;;  #sed -i "${STARTLINE},${ENDLINE}d" ${BASH_RC}
+            Cancel ) change_menu;;
+        esac
+    done
+
+    printf "${green}Successfuly uninstalled, your bash is now boring again.${colorless}\n"
+    exit
+}
+
+function unlink_from_dropbox() {
+    echo "Unlink"
+}
+
+function link_to_dropbox() {
+    echo "Link"
+}
+
+function toggle_dropbox() {
+    case $1 in
+        Unlink) unlink_from_dropbox;;
+        Link) link_to_dropbox;;
+        Cancel ) change_menu;;
+    esac
+}
+
+function change_menu() {
+    printf "What would you like to do? \n"
+     # Check if path to "Dropbox" is in ${HISTPATH}
+    case ${HISTPATH} in
+        *Dropbox*)
+            DBOXOPTION=Unlink;;
+        *)  DBOXOPTION=Link;;
+    esac
+
+    select option in "Uninstall" "${DBOXOPTION} from dropbox" "Exit"; do
+        case ${option} in
+            Uninstall) uninstall;;
+            "${DBOXOPTION} from dropbox") toggle_dropbox ${DBOXOPTION};;
+            Exit ) exit;;
+        esac
+    done
+}
+
+function make_bash_rc_backup() {
+  printf "Making a copy of your ${BASH_RC} file to ${BASH_RC}_backup.\n"
+  cp ${BASH_RC} ${BASH_RC}_backup
+}
+
 if [ -f ${BASH_RC} ]; then
   if grep -q "${HISTFILE_NAME}" "${BASH_RC}"; then
-    echo "It looks like script has already been run, no changes will be made."
-    exit 1
+    change_menu
   fi
-  echo "Making a copy of your ${BASH_RC} file to ${BASH_RC}_backup."
-  cp ${BASH_RC} ${BASH_RC}_backup
-  echo "Comment out old occurances of HISTSIZE and HISTFILESIZE."
-  sed -i s/^HISTSIZE\=/#HISTSIZE\=/g ${BASH_RC}
-  sed -i s/^HISTFILESIZE\=/#HISTFILESIZE\=/g ${BASH_RC}
+  exit #devguard
+
 fi
 
-ETERNAL_BASH_SNIPPET="
-# Eternal bash history - version : ${VERSION}
-# ---------------------
-# Undocumented feature which sets the size to 'unlimited'.
-# http://stackoverflow.com/questions/9457233/unlimited-bash-history
-export HISTFILESIZE=
-export HISTSIZE=
-export HISTTIMEFORMAT=\"[%F %T] \"
-# Force prompt to write history after every command. See 'help history'
-PROMPT_COMMAND=\"history -a; history -c; history -r; \${PROMPT_COMMAND}\""
+function append_snippet() {
+    ETERNAL_BASH_SNIPPET="
+    # Eternal bash history
+    # ---------------------
+    # Undocumented feature which sets the size to 'unlimited'.
+    # http://stackoverflow.com/questions/9457233/unlimited-bash-history
+    export HISTFILESIZE=
+    export HISTSIZE=
+    export HISTTIMEFORMAT=\"[%F %T] \"
+    # Force prompt to write history after every command. See 'help history'
+    PROMPT_COMMAND=\"history -a; history -c; history -r; \${PROMPT_COMMAND}\""
 
-# Important with quotes here, so the snippet will retain line-breaks
-echo "${ETERNAL_BASH_SNIPPET}" >> ${BASH_RC}
+    # Important with quotes here, so the snippet will retain line-breaks
+    echo "${ETERNAL_BASH_SNIPPET}" >> ${BASH_RC}
+}
 
+
+### TODO Moving this to link to dropbox
 if [ -d "${DROPBOX_DEFAULT_LOCATION}" ]; then
   echo "Dropbox found at default location: ${DROPBOX_DEFAULT_LOCATION}"
   HISTFILE_LOCATION=${DROPBOX_DEFAULT_LOCATION}/${DROPBOX_FOLDER}/${HISTFILE_NAME}
@@ -92,16 +168,19 @@ if [ -d "${DROPBOX_DEFAULT_LOCATION}" ]; then
     echo export HISTFILE=~/${HISTFILE_NAME} >> ${BASH_RC}
   fi
 else
+  ### TODO Move this to unlink
   HISTFILE_LOCATION=~/${HISTFILE_NAME}
   echo "Dropbox not found, history file is set to ${HISTFILE_LOCATION}"
   echo export HISTFILE=${HISTFILE_LOCATION} >> ${BASH_RC}
 fi
 
-if [ -f ${OLD_HISTORY} ]; then
-  echo "Appending ${OLD_HISTORY} to ${HISTFILE_LOCATION}"
-  cat ${OLD_HISTORY} >> ${HISTFILE_LOCATION}
+### TODO read each line of ${CURREN_HISTFILE} append this NOT in ${HISTFILE_LOCATION}
+if [ -f ${CURRENT_HISTFILE} ]; then
+  echo "Appending ${CURRENT_HISTFILE} to ${HISTFILE_LOCATION}"
+  cat ${CURRENT_HISTFILE} >> ${HISTFILE_LOCATION}
 fi
 
+### TODO move this to function
 # OS X does not automatically run .bashrc, add it as source to .bash_profile.
 if [ $(uname) = "Darwin" ]; then
   echo "OS X detected, adding source to ${BASH_RC} in .bash_profile"
